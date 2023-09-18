b8d76bff2fdec76a170517c40b8a97a1f06d6321
Tobeyw <tobey1024@126.com>
2023-09-18 16:39:15 +0800

    remove blacklist variable

--- contracts/solidity/Policy.sol
+++ contracts/solidity/Policy.sol
@@ -1,18 +1,16 @@
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./GovernanceVote.sol";
 
 contract Policy is GovernanceVote{
-    using EnumerableSet for EnumerableSet.AddressSet;   
+   
     uint public  minGasPrice ;
-    mapping(address=>bool) public isBlackListed;
-    EnumerableSet.AddressSet  blackList;
+    mapping(address=>bool) public isBlackListed;    
     bool private initialized;
    
 
-    function initialize()public{
+    function initialize() public {
         require(!initialized, "Contract instance has already been initialized");
         minGasPrice = 10000000;       
         initialized = true;
@@ -24,20 +22,16 @@ contract Policy is GovernanceVote{
         minGasPrice = _gasPrice;    
     }   
 
-    //  add/cancel blacklist
-    function setBlackList (address _addr,bool _isBlackListed) external needVote(keccak256("setBlackList"), keccak256(abi.encode(_addr,_isBlackListed))) {
-        isBlackListed[_addr] = _isBlackListed;  
-        if (_isBlackListed){
-           require( blackList.add(_addr), "Policy: blacklisted add error");
-        }else{
-           require( blackList.remove(_addr), "Policy: blacklisted remove error");
-        }
+    //  cancel blacklist
+    function addBlackList (address _addr) external needVote(keccak256("setBlackList"), keccak256(abi.encode(_addr))) {
+        require(!isBlackListed[_addr],"Policy: Blacklist already exists");
+        isBlackListed[_addr] = true;        
+    }
+    //  cancel blacklist
+    function removeBlackList (address _addr) external needVote(keccak256("setBlackList"), keccak256(abi.encode(_addr))) {
+        require(isBlackListed[_addr],"Policy: Blacklist does not exist");
+        isBlackListed[_addr] = false;        
            
     }
 
-    // get blacklist
-    function getBlackList() public view returns (address[] memory){  
-        return blackList.values();
-    }
- 
 }
