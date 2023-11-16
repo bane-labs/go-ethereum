83207a6be6257452ded3f725f7834efc5a17442a
Anna Shaleva <shaleva.ann@nspcc.ru>
2023-11-16 11:00:15 +0300

    dbft: rename package from `clique` to `dbft`
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

--- consensus/dbft/dbft.go
+++ consensus/dbft/dbft.go
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package clique implements the proof-of-authority consensus engine.
-package clique
+// Package dbft implements the proof-of-authority consensus engine.
+package dbft
 
 import (
 	"bytes"
