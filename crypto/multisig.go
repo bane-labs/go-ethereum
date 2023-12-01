64f001617819c8658ebb8737e1d16aae0515b646
Roman Khimov <roman@nspcc.ru>
2023-12-01 23:44:15 +0300

    crypto: add basic multisig primitives
    Can be optimized in many ways, but enough to have something for the beginning.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

---
+++ crypto/multisig.go
@@ -0,0 +1,87 @@
+// Copyright 2023 NeoSPCC
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package crypto
+
+import (
+	"bytes"
+	"errors"
+	"sort"
+)
+
+// VerifyMultiBFT checks for BFT (> 2/3) number of sigs made by pubs for hash.
+func VerifyMultiBFT(hash []byte, pubs [][]byte, sigs [][]byte) error {
+	var (
+		n = len(pubs)
+		m = n - (n-1)/3
+	)
+	if len(sigs) != m {
+		return errors.New("wrong number of signatures")
+	}
+	return VerifyMulti(hash, pubs, sigs)
+}
+
+// VerifyMultiBFT checks for majority (> 1/2) number of sigs made by pubs for hash.
+func VerifyMultiMajority(hash []byte, pubs [][]byte, sigs [][]byte) error {
+	var (
+		n = len(pubs)
+		m = n - (n-1)/2
+	)
+	if len(sigs) != m {
+		return errors.New("wrong number of signatures")
+	}
+	return VerifyMulti(hash, pubs, sigs)
+}
+
+// VerifyMulti verifies that hash was signed by a subset of keys specified
+// in pubs. Checking the number of sigs is out of scope, see [VerifyMultiBFT]
+// and [VerifyMultiMajority].
+func VerifyMulti(hash []byte, pubs [][]byte, sigs [][]byte) error {
+	if len(pubs) < len(sigs) {
+		return errors.New("number of public keys and signatures doesn't match")
+	}
+
+	var (
+		vPubs = make([][]byte, len(pubs))
+		sPubs = make([][]byte, 0, len(pubs))
+	)
+	copy(vPubs, pubs)
+	sort.Slice(vPubs, func(i, j int) bool {
+		return bytes.Compare(vPubs[i], vPubs[j]) < 0
+	})
+	for i := range sigs {
+		pubkey, err := Ecrecover(hash, sigs[i])
+		if err != nil {
+			return err
+		}
+		sPubs = append(sPubs, pubkey)
+	}
+	sort.Slice(sPubs, func(i, j int) bool {
+		return bytes.Compare(sPubs[i], sPubs[j]) < 0
+	})
+	var vi int
+	for si := range sPubs {
+		for ; vi < len(vPubs); vi++ {
+			if bytes.Compare(vPubs[vi], sPubs[si]) == 0 {
+				break
+			}
+		}
+		if vi == len(vPubs) {
+			return errors.New("invalid signature")
+		}
+	}
+	return nil
+}
