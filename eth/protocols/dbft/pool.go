915245b04088e37d1e58438e86a8ac168531dda8
Roman Khimov <roman@nspcc.ru>
2023-12-04 16:28:46 +0300

    dbft: add message pool
    Based on NeoGo, no extensibles, just pure dBFT data.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

---
+++ eth/protocols/dbft/pool.go
@@ -0,0 +1,133 @@
+// Copyright 2023 NeoSPCC
+//
+// MIT License.
+package dbft
+
+import (
+	"container/list"
+	"errors"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// Ledger is enough of Blockchain to satisfy Pool.
+type Ledger interface {
+	BlockHeight() uint32
+	IsAddressAllowed(common.Address) bool
+}
+
+// Pool represents a pool of extensible payloads.
+type Pool struct {
+	lock     sync.RWMutex
+	verified map[common.Hash]*list.Element
+	senders  map[common.Address]*list.List
+	// singleCap represents the maximum number of payloads from a single sender.
+	singleCap int
+	chain     Ledger
+}
+
+// New returns a new payload pool using the provided chain.
+func New(bc Ledger, capacity int) *Pool {
+	if capacity <= 0 {
+		panic("invalid capacity")
+	}
+
+	return &Pool{
+		verified:  make(map[common.Hash]*list.Element),
+		senders:   make(map[common.Address]*list.List),
+		singleCap: capacity,
+		chain:     bc,
+	}
+}
+
+var (
+	errDisallowedSender = errors.New("disallowed sender")
+	errInvalidHeight    = errors.New("invalid height")
+)
+
+// Add adds an extensible payload to the pool.
+// First return value specifies if the payload was new.
+// Second one is nil if and only if the payload is valid.
+func (p *Pool) Add(m *Message) (bool, error) {
+	if ok, err := p.verify(m); err != nil || !ok {
+		return ok, err
+	}
+
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	h := m.Hash()
+	if _, ok := p.verified[h]; ok {
+		return false, nil
+	}
+
+	lst, ok := p.senders[m.Sender]
+	if ok && lst.Len() >= p.singleCap {
+		value := lst.Remove(lst.Front())
+		delete(p.verified, value.(*Message).Hash())
+	} else if !ok {
+		lst = list.New()
+		p.senders[m.Sender] = lst
+	}
+
+	p.verified[h] = lst.PushBack(m)
+	return true, nil
+}
+
+func (p *Pool) verify(m *Message) (bool, error) {
+	err := m.Verify()
+	if err != nil {
+		return false, err
+	}
+
+	h := p.chain.BlockHeight()
+	if h < m.ValidBlockStart || m.ValidBlockEnd <= h {
+		// We can receive a consensus payload for the last or next block
+		// which leads to an unwanted node disconnect.
+		if m.ValidBlockEnd == h {
+			return false, nil
+		}
+		return false, errInvalidHeight
+	}
+	if !p.chain.IsAddressAllowed(m.Sender) {
+		return false, errDisallowedSender
+	}
+	return true, nil
+}
+
+// Get returns payload by hash.
+func (p *Pool) Get(h common.Hash) *Message {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+
+	elem, ok := p.verified[h]
+	if !ok {
+		return nil
+	}
+	return elem.Value.(*Message)
+}
+
+// RemoveStale removes invalid payloads after block processing.
+func (p *Pool) RemoveStale(index uint32) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	for s, lst := range p.senders {
+		for elem := lst.Front(); elem != nil; {
+			m := elem.Value.(*Message)
+			h := m.Hash()
+			old := elem
+			elem = elem.Next()
+
+			if m.ValidBlockEnd <= index || !p.chain.IsAddressAllowed(m.Sender) {
+				delete(p.verified, h)
+				lst.Remove(old)
+				continue
+			}
+		}
+		if lst.Len() == 0 {
+			delete(p.senders, s)
+		}
+	}
+}
---
+++ eth/protocols/dbft/pool_test.go
@@ -0,0 +1,149 @@
+// Copyright 2023 NeoSPCC
+//
+// MIT License.
+package dbft
+
+import (
+	"crypto/ecdsa"
+	"errors"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddGet(t *testing.T) {
+	bc := newTestChain()
+	bc.height = 10
+
+	p := New(bc, 100)
+	t.Run("invalid witness", func(t *testing.T) {
+		m := someMessage(t, 100, bc.goodAddrs[0], bc.badKey)
+		p.testAdd(t, false, invalidSig, m)
+	})
+	t.Run("disallowed sender", func(t *testing.T) {
+		m := bc.badMessage(t, 100)
+		p.testAdd(t, false, errDisallowedSender, m)
+	})
+	t.Run("bad height", func(t *testing.T) {
+		m := bc.goodMessage(t, 9)
+		p.testAdd(t, false, errInvalidHeight, m)
+
+		m = bc.goodMessage(t, 10)
+		p.testAdd(t, false, nil, m)
+	})
+	t.Run("good", func(t *testing.T) {
+		m := bc.goodMessage(t, 11)
+
+		p.testAdd(t, true, nil, m)
+		require.Equal(t, m, p.Get(m.Hash()))
+		p.testAdd(t, false, nil, m)
+	})
+}
+
+func TestCapacityLimit(t *testing.T) {
+	bc := newTestChain()
+	bc.height = 10
+
+	t.Run("invalid capacity", func(t *testing.T) {
+		require.Panics(t, func() { New(bc, 0) })
+	})
+
+	p := New(bc, 3)
+
+	first := bc.goodMessage(t, 11)
+	p.testAdd(t, true, nil, first)
+
+	for _, height := range []uint32{12, 13} {
+		m := bc.goodMessage(t, height)
+		p.testAdd(t, true, nil, m)
+	}
+
+	require.NotNil(t, p.Get(first.Hash()))
+
+	ok, err := p.Add(bc.goodMessage(t, 14))
+	require.True(t, ok)
+	require.NoError(t, err)
+
+	require.Nil(t, p.Get(first.Hash()))
+}
+
+func TestRemoveStale(t *testing.T) {
+	bc := newTestChain()
+	bc.height = 10
+	bc.goodAddrs = append(bc.goodAddrs, crypto.PubkeyToAddress(bc.badKey.PublicKey))
+
+	p := New(bc, 100)
+	eps := []*Message{
+		bc.goodMessage(t, 11), // small height
+		bc.goodMessage(t, 12), // good
+		bc.badMessage(t, 12),  // invalid sender
+	}
+	for i := range eps {
+		p.testAdd(t, true, nil, eps[i])
+	}
+	bc.goodAddrs = bc.goodAddrs[:1] // drop bad key again
+	p.RemoveStale(11)
+	require.Nil(t, p.Get(eps[0].Hash()))
+	require.Equal(t, eps[1], p.Get(eps[1].Hash()))
+	require.Nil(t, p.Get(eps[2].Hash()))
+}
+
+func (p *Pool) testAdd(t *testing.T, expectedOk bool, expectedErr error, ep *Message) {
+	ok, err := p.Add(ep)
+	if expectedErr != nil {
+		require.ErrorIs(t, err, expectedErr)
+	} else {
+		require.NoError(t, err)
+	}
+	require.Equal(t, expectedOk, ok)
+}
+
+type testChain struct {
+	height    uint32
+	goodKey   *ecdsa.PrivateKey
+	badKey    *ecdsa.PrivateKey
+	goodAddrs []common.Address
+	badAddr   common.Address
+}
+
+var errVerification = errors.New("verification failed")
+
+func newTestChain() *testChain {
+	gk, _ := crypto.GenerateKey()
+	bk, _ := crypto.GenerateKey()
+	return &testChain{
+		goodKey:   gk,
+		badKey:    bk,
+		goodAddrs: []common.Address{crypto.PubkeyToAddress(gk.PublicKey)},
+		badAddr:   crypto.PubkeyToAddress(bk.PublicKey),
+	}
+}
+func (c *testChain) IsAddressAllowed(u common.Address) bool {
+	for i := range c.goodAddrs {
+		if u == c.goodAddrs[i] {
+			return true
+		}
+	}
+	return false
+}
+func (c *testChain) BlockHeight() uint32 { return c.height }
+
+func (c *testChain) goodMessage(t *testing.T, height uint32) *Message {
+	return someMessage(t, height, c.goodAddrs[0], c.goodKey)
+}
+
+func (c *testChain) badMessage(t *testing.T, height uint32) *Message {
+	return someMessage(t, height, c.badAddr, c.badKey)
+}
+
+func someMessage(t *testing.T, height uint32, sender common.Address, signer *ecdsa.PrivateKey) *Message {
+	m := &Message{ValidBlockEnd: height, Sender: sender, Data: []byte{42}}
+	h := m.Hash()
+	sig, err := crypto.Sign(h[:], signer)
+	require.NoError(t, err)
+	m.Witness = sig
+
+	return m
+}
