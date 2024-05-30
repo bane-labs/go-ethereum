3a6aaa5b5aec013b325bad94ca5faab5bdc90853
Michael Bucher <michael@axlabs.com>
2024-05-30 10:01:43 +0200

    systemcontract: add native treasury for bridge (#184)
    * (#183) Add draft implementation for native treasury
    
    * (#183) Use Errors.TransferFailed if funding transfer failed
    
    * (#183) Update treasury pragma to ^0.8.25
    
    * (#183) Use direct hash for needVote in fundBridge function of Treasury
    
    * (#183) Add ITreasury
    
    * (#183) Rename fund event and remove redundant error declaration in treasury contract

---
+++ contracts/solidity/Treasury.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "./GovernanceVote.sol";
+
+interface ITreasury {
+    event BridgeFund(uint256 amount);
+
+    function fundBridge(uint256 _amount) external;
+}
+
+/**
+ * @dev This is an auxiliary contract meant to be assigned as the Neo X treasury for funding the native bridge proxy.
+ * Use GovernanceVote to manage funding the bridge.
+ */
+contract Treasury is GovernanceVote, ITreasury {
+    address public constant BRIDGE_PROXY =
+        0x1212000000000000000000000000000000000004;
+
+    /**
+     * @dev Sends `amount` of ether to the bridge proxy.
+     */
+    function fundBridge(
+        uint256 _amount
+    )
+        external
+        needVote(
+            bytes32(
+                0xdd6d322687f552c30b168d744bbd29145a2095a3557a58387f7e7230c9449179
+            ),
+            keccak256(abi.encode(_amount))
+        )
+    {
+        (bool success, ) = BRIDGE_PROXY.call{value: _amount}("");
+        if (!success) {
+            revert Errors.TransferFailed();
+        }
+        emit BridgeFund(_amount);
+    }
+}
