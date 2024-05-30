c51c9088f2e0c434053237947adb1f85100f99db
txhsl <799498265@qq.com>
2024-05-30 11:39:27 +0800

    rename constants to libraries

--- contracts/solidity/base/GovProxyUpgradeable.sol
+++ contracts/solidity/base/GovProxyUpgradeable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import "../constants/Errors.sol";
+import "../libraries/Errors.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 abstract contract GovProxyUpgradeable is UUPSUpgradeable {
--- contracts/solidity/base/GovernanceVote.sol
+++ contracts/solidity/base/GovernanceVote.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import "../constants/Errors.sol";
+import "../libraries/Errors.sol";
 import "../interfaces/IGovReward.sol";
 
 abstract contract GovernanceVote {
