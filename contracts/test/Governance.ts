bane-labs/go-ethereum

commit 5af9d7c4500a6ee9b6c69f85d8c91070255a9560

systemcontract: use customized error library (#209)
* systemcontract: use customized error library

* update existing ut

contracts/solidity/Governance.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import "./Errors.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
@@ -103,7 +104,7 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
     mapping(address => mapping(uint => uint)) public epochStartGasPerVote;
 
     modifier onlyAdmin() {
-        require(msg.sender == GOV_ADMIN, "not admin");
+        if (msg.sender != GOV_ADMIN) revert Errors.NotAdmin();
         _;
     }
 
@@ -132,7 +133,7 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
     }
 
     receive() external payable nonReentrant {
-        require(msg.sender == GOV_REWARD, "side call not allowed");
+        if (msg.sender != GOV_REWARD) revert Errors.SideCallNotAllowed();
         address[] memory validators = currentConsensus;
         uint length = validators.length;
         for (uint i = 0; i < length; i++) {
@@ -157,11 +158,11 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
     }
 
     function registerCandidate(uint shareRate) external payable {
-        require(tx.origin == msg.sender, "only allow EOA");
-        require(msg.value >= registerFee, "insufficient amount");
-        require(shareRate < 1000, "invalid rate");
-        require(!candidateList.contains(msg.sender), "candidate exists");
-        require(exitHeightOf[msg.sender] == 0, "left not claimed");
+        if (tx.origin != msg.sender) revert Errors.OnlyEOA();
+        if (msg.value < registerFee) revert Errors.InsufficientValue();
+        if (shareRate > 1000) revert Errors.InvalidShareRate();
+        if (candidateList.contains(msg.sender)) revert Errors.CandidateExists();
+        if (exitHeightOf[msg.sender] > 0) revert Errors.LeftNotClaimed();
         candidateList.add(msg.sender);
         if (receivedVotes[msg.sender] > 0) {
             totalVotes += receivedVotes[msg.sender];
@@ -174,7 +175,8 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
     }
 
     function exitCandidate() external {
-        require(candidateList.contains(msg.sender), "candidate not exists");
+        if (!candidateList.contains(msg.sender))
+            revert Errors.CandidateNotExists();
         // remove candidate list, balance still locked
         candidateList.remove(msg.sender);
         exitHeightOf[msg.sender] = block.number;
@@ -187,11 +189,10 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
     function withdrawRegisterFee() external nonReentrant {
         // require 2 epochs to exit candidate list
         // NOTE: suppose epoch change always happens in time
-        require(
-            exitHeightOf[msg.sender] > 0 &&
-                block.number > exitHeightOf[msg.sender] + 2 * epochDuration,
-            "withdraw not allowed"
-        );
+        if (
+            exitHeightOf[msg.sender] <= 0 ||
+            block.number <= exitHeightOf[msg.sender] + 2 * epochDuration
+        ) revert Errors.CandidateWithdrawNotAllowed();
 
         // send back balance
         uint amount = candidateBalanceOf[msg.sender];
@@ -204,13 +205,12 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
     }
 
     function vote(address candidateTo) external payable nonReentrant {
-        require(msg.value >= minVoteAmount, "insufficient amount");
-        require(candidateList.contains(candidateTo), "candidate not allowed");
+        if (msg.value < minVoteAmount) revert Errors.InsufficientValue();
+        if (!candidateList.contains(candidateTo))
+            revert Errors.CandidateNotExists();
         address votedCandidate = votedTo[msg.sender];
-        require(
-            votedCandidate == candidateTo || votedCandidate == address(0),
-            "only one choice is allowed"
-        );
+        if (votedCandidate != candidateTo && votedCandidate != address(0))
+            revert Errors.MultipleVoteNotAllowed();
 
         // settle reward here
         uint unclaimedReward = 0;
@@ -238,10 +238,7 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
     function revokeVote() external nonReentrant {
         address candidateFrom = votedTo[msg.sender];
         uint amount = votedAmount[msg.sender];
-        require(
-            candidateFrom != address(0) && amount > 0,
-            "revoke not allowed"
-        );
+        if (candidateFrom == address(0) || amount <= 0) revert Errors.NoVote();
 
         // settle reward here
         uint unclaimedReward = _settleReward(msg.sender, candidateFrom);
@@ -265,7 +262,7 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
 
     function claimReward() external nonReentrant {
         address votedCandidate = votedTo[msg.sender];
-        require(votedCandidate != address(0), "claim not allowed");
+        if (votedCandidate == address(0)) revert Errors.NoVote();
         uint unclaimedReward = _settleReward(msg.sender, votedCandidate);
         if (unclaimedReward > 0) _safeTransferETH(msg.sender, unclaimedReward);
     }
@@ -278,7 +275,7 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
 
     function onPersist() external {
         // NOTE: suppose onPersist always happens at the beginning of every block
-        require(msg.sender == SYS_CALL, "side call not allowed");
+        if (msg.sender != SYS_CALL) revert Errors.SideCallNotAllowed();
         // only settle validator reward if there is no epoch change
         IGovReward(GOV_REWARD).withdraw();
         if (block.number < currentEpochStartHeight + epochDuration) return;
@@ -341,7 +338,7 @@ contract Governance is IGovernance, ReentrancyGuard, UUPSUpgradeable {
 
     function _safeTransferETH(address to, uint value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
-        require(success, "safeTransferETH: ETH transfer failed");
+        if (!success) revert Errors.TransferFailed();
     }
 
     function _computeConsensus() internal view returns (address[] memory) {

contracts/test/Governance.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import { mine } from "@nomicfoundation/hardhat-network-helpers";
-
+import { ERRORS } from "./helpers/errors";
 
 // NATIVE ADDRESSES
 const GOV_ADMIN = "0x1212000000000000000000000000000000000000";
@@ -106,7 +106,7 @@ describe("Governance", function () {
 
             await expect(
                 contract.call_registerCandidate(Governance, 500, { value: REGISTER_FEE })
-            ).to.be.revertedWith("only allow EOA");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.ONLY_EOA);
         });
 
         it("Should revert if the sender is already a candidate", async function () {
@@ -116,19 +116,19 @@ describe("Governance", function () {
                 Governance
                     .connect(candidate1)
                     .registerCandidate(500, { value: REGISTER_FEE })
-            ).to.be.revertedWith("candidate exists");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.CANDIDATE_EXISTS);
         });
 
         it("Should revert if the value sent is less than the registration fee", async function () {
             await expect(
                 Governance.connect(candidate1).registerCandidate(500, { value: REGISTER_FEE - BigInt(1) })
-            ).to.be.revertedWith("insufficient amount");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.INSUFFICIENT_VALUE);
         });
 
         it("Should revert if register a candidate with more than 1000 shareRate", async function () {
             await expect(
                 Governance.connect(candidate1).registerCandidate(1001, { value: REGISTER_FEE })
-            ).to.be.revertedWith("invalid rate");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.INVALID_SHARE_RATE);
         });
 
         it("Should register a new candidate if all conditions are met", async function () {
@@ -152,7 +152,7 @@ describe("Governance", function () {
         it("Should revert if the sender is not a candidate", async function () {
             await expect(
                 Governance.connect(candidate1).exitCandidate()
-            ).to.be.revertedWith("candidate not exists");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.CANDIDATE_NOT_EXISTS);
         });
 
         it("Should remove a candidate if all conditions are met", async function () {
@@ -183,7 +183,7 @@ describe("Governance", function () {
         it("Should revert if the sender is not a candidate", async function () {
             await expect(
                 Governance.connect(candidate1).withdrawRegisterFee()
-            ).to.be.revertedWith("withdraw not allowed");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.CANDIDATE_WITHDRAW_NOT_ALLOWED);
         });
         it("Should revert if the sender has not exited", async function () {
             await expect(
@@ -192,7 +192,7 @@ describe("Governance", function () {
 
             await expect(
                 Governance.connect(candidate1).withdrawRegisterFee()
-            ).to.be.revertedWith("withdraw not allowed");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.CANDIDATE_WITHDRAW_NOT_ALLOWED);
         });
         it("Should revert if the sender has not waited for 2 epochs", async function () {
             await expect(
@@ -204,7 +204,7 @@ describe("Governance", function () {
 
             await expect(
                 Governance.connect(candidate1).withdrawRegisterFee()
-            ).to.be.revertedWith("withdraw not allowed");
+            ).to.be.revertedWithCustomError(Governance, ERRORS.CANDIDATE_WITHDRAW_NOT_ALLOWED);
         });
         it("Should transfer back register fee if all conditions are met", async function () {
             await expect(
@@ -310,8 +310,9 @@ describe("Governance", function () {
         });
 
         it("Should revert if caller is not SYS_CALL", async function () {
-            await expect(Governance.connect(user).onPersist()).to.be.revertedWith(
-                "side call not allowed"
+            await expect(Governance.connect(user).onPersist()).to.be.revertedWithCustomError(
+                Governance,
+                ERRORS.SIDE_CALL_OT_ALLOWED
             );
         });
 

contracts/solidity/Errors.sol
@@ -1,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+library Errors {
+    // Universal Errors
+    error NotAdmin();
+    error NotGovernance();
+
+    // GovReward Errors
+    error TransferFailed();
+
+    // GovernanceVote Errors
+    error NotMiner();
+
+    // Policy Errors
+    error BlacklistExists();
+    error BlacklistNotExists();
+    error InvalidMinGasTipCap();
+    error InvalidBaseFee();
+
+    // Governance Errors
+    error SideCallNotAllowed();
+    error OnlyEOA();
+    error InsufficientValue();
+    error InvalidShareRate();
+    error CandidateExists();
+    error CandidateNotExists();
+    error LeftNotClaimed();
+    error CandidateWithdrawNotAllowed();
+    error MultipleVoteNotAllowed();
+    error NoVote();
+}

contracts/solidity/GovReward.sol
@@ -1,25 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import "./Errors.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-
-library TransferHelper {
-    function safeTransfer(address token, address to, uint256 value) internal {
-        // bytes4(keccak256(bytes('transfer(address,uint256)')));
-        (bool success, bytes memory data) = token.call(
-            abi.encodeWithSelector(0xa9059cbb, to, value)
-        );
-        require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
-            "safeTransfer: transfer failed"
-        );
-    }
-
-    function safeTransferETH(address to, uint256 value) internal {
-        (bool success, ) = to.call{value: value}(new bytes(0));
-        require(success, "safeTransferETH: ETH transfer failed");
-    }
-}
 
 interface IGovernance {
     // get current consensus group
@@ -42,12 +25,12 @@ contract GovReward is IGovReward, UUPSUpgradeable {
     receive() external payable {}
 
     modifier onlyGov() {
-        require(msg.sender == GOV, "not governance");
+        if (msg.sender != GOV) revert Errors.NotGovernance();
         _;
     }
 
     modifier onlyAdmin() {
-        require(msg.sender == GOV_ADMIN, "not admin");
+        if (msg.sender != GOV_ADMIN) revert Errors.NotAdmin();
         _;
     }
 
@@ -81,7 +64,12 @@ contract GovReward is IGovReward, UUPSUpgradeable {
 
     function withdraw() external onlyGov {
         if (address(this).balance > 0) {
-            TransferHelper.safeTransferETH(GOV, address(this).balance);
+            _safeTransferETH(GOV, address(this).balance);
         }
+    }
+
+    function _safeTransferETH(address to, uint value) internal {
+        (bool success, ) = to.call{value: value}(new bytes(0));
+        if (!success) revert Errors.TransferFailed();
     }
 }

contracts/solidity/GovernanceVote.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import "./Errors.sol";
+
 interface IGovReward {
     function getMiners() external view returns (address[] memory);
 }
@@ -54,7 +56,7 @@ abstract contract GovernanceVote {
     }
 
     modifier needVote(bytes32 methodKey, bytes32 paramKey) {
-        require(isMiner(msg.sender), "not Miner");
+        if(!isMiner(msg.sender)) revert Errors.NotMiner();
         // update vote map
         vote(methodKey, paramKey);
         // check vote, if not pass just return

contracts/solidity/Policy.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
+import "./Errors.sol";
 import "./GovernanceVote.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
@@ -19,7 +20,7 @@ contract Policy is GovernanceVote, UUPSUpgradeable {
     event SetBaseFee(uint256 baseFee);
 
     modifier onlyAdmin() {
-        require(msg.sender == GOV_ADMIN, "not admin");
+        if (msg.sender != GOV_ADMIN) revert Errors.NotAdmin();
         _;
     }
 
@@ -54,7 +55,7 @@ contract Policy is GovernanceVote, UUPSUpgradeable {
         external
         needVote(keccak256("addBlackList"), keccak256(abi.encode(_addr)))
     {
-        require(!isBlackListed[_addr], "Policy: Blacklist already exists");
+        if (isBlackListed[_addr]) revert Errors.BlacklistExists();
         isBlackListed[_addr] = true;
         emit AddBlackList(_addr);
     }
@@ -66,7 +67,7 @@ contract Policy is GovernanceVote, UUPSUpgradeable {
         external
         needVote(keccak256("removeBlackList"), keccak256(abi.encode(_addr)))
     {
-        require(isBlackListed[_addr], "Policy: Blacklist does not exist");
+        if (!isBlackListed[_addr]) revert Errors.BlacklistNotExists();
         delete isBlackListed[_addr];
         emit RemoveBlackList(_addr);
     }
@@ -81,7 +82,7 @@ contract Policy is GovernanceVote, UUPSUpgradeable {
             keccak256(abi.encode(_gasTipCap))
         )
     {
-        require(_gasTipCap > 0, "Policy: setMinGasTipCap invalid parameter");
+        if (_gasTipCap <= 0) revert Errors.InvalidMinGasTipCap();
         minGasTipCap = _gasTipCap;
         emit SetMinGasTipCap(_gasTipCap);
     }
@@ -93,7 +94,7 @@ contract Policy is GovernanceVote, UUPSUpgradeable {
         external
         needVote(keccak256("setBaseFee"), keccak256(abi.encode(_baseFee)))
     {
-        require(_baseFee > 0, "Policy: setBaseFee invalid parameter");
+        if (_baseFee <= 0) revert Errors.InvalidBaseFee();
         baseFee = _baseFee;
         emit SetBaseFee(_baseFee);
     }

contracts/test/helpers/errors.ts
@@ -1,0 +1,20 @@
+export const ERRORS = {
+    NOT_ADMIN: 'NotAdmin()',
+    NOT_GOVERNANCE: 'NotGovernance()',
+    TRANSFER_FAILED: 'TransferFailed()',
+    NOT_MINER: 'NotMiner()',
+    BLACKLIST_EXISTS: 'BlacklistExists()',
+    BLACKLIST_NOT_EXISTS: 'BlacklistNotExists()',
+    INVALID_MIN_GAS_TIP_CAP: 'InvalidMinGasTipCap()',
+    INVALID_BASE_FEE: 'InvalidBaseFee()',
+    SIDE_CALL_OT_ALLOWED: 'SideCallNotAllowed()',
+    ONLY_EOA: 'OnlyEOA()',
+    INSUFFICIENT_VALUE: 'InsufficientValue()',
+    INVALID_SHARE_RATE: 'InvalidShareRate()',
+    CANDIDATE_EXISTS: 'CandidateExists()',
+    CANDIDATE_NOT_EXISTS: 'CandidateNotExists()',
+    LEFT_NOT_CLAIMED: 'LeftNotClaimed()',
+    CANDIDATE_WITHDRAW_NOT_ALLOWED: 'CandidateWithdrawNotAllowed()',
+    MULTIPLE_VOTE_NOT_ALLOWED: 'MultipleVoteNotAllowed()',
+    NO_VOTE: 'NoVote()',
+};

commit a96f3a54e72f0d55508214b65f6510d470bba801

systemcontract: fix potential zero division error may caused by #209 (#225)

contracts/solidity/Governance.sol
@@ -6,7 +6,7 @@ import {IGovReward} from "./interfaces/IGovReward.sol";
 import {IGovernance} from "./interfaces/IGovernance.sol";
 import {ERC1967Utils, GovProxyUpgradeable} from "./base/GovProxyUpgradeable.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import {ReentrancyGuard} from"@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Governance is IGovernance, ReentrancyGuard, GovProxyUpgradeable {
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -87,19 +87,21 @@ contract Governance is IGovernance, ReentrancyGuard, GovProxyUpgradeable {
         address[] memory validators = currentConsensus;
         uint length = validators.length;
         for (uint i = 0; i < length; i++) {
-            if (receivedVotes[validators[i]] != 0) {
+            uint shareRate = shareRateOf[validators[i]];
+            uint voteAmount = receivedVotes[validators[i]];
+            if (voteAmount != 0) {
                 candidateGasPerVote[validators[i]] +=
-                    (msg.value * shareRateOf[validators[i]] * SCALE_FACTOR) /
+                    (msg.value * shareRate * SCALE_FACTOR) /
                     consensusSize /
                     1000 /
-                    receivedVotes[validators[i]];
+                    voteAmount;
             }
-            _safeTransferETH(
-                validators[i],
-                (msg.value * (1000 - shareRateOf[validators[i]])) /
-                    consensusSize /
-                    1000
-            );
+            if (shareRate < 1000) {
+                _safeTransferETH(
+                    validators[i],
+                    (msg.value * (1000 - shareRate)) / consensusSize / 1000
+                );
+            }
         }
     }
 

contracts/test/Governance.ts
@@ -590,6 +590,31 @@ describe("Governance", function () {
             ).to.eq(166666666666666);
         });
 
+        it("Should work with 1000 share rate and 0 votes", async function () {
+            // Register enough candidates and change consensus
+            let signers = await ethers.getSigners();
+            await Governance.connect(signers[0]).registerCandidate(500, { value: REGISTER_FEE });
+            await Governance.connect(signers[1]).registerCandidate(1000, { value: REGISTER_FEE });
+            for (let i = 2; i < CONSENSUS_SIZE; i++) {
+                await Governance.connect(signers[i]).registerCandidate(500, { value: REGISTER_FEE });
+            }
+            for (let i = 1; i < CONSENSUS_SIZE; i++) {
+                await Governance.connect(signers[i]).vote(signers[i], { value: VOTE_TARGET_AMOUNT });
+            }
+            await mine(EPOCH_DURATION);
+            await MockSysCall.call_onPersist(Governance);
+
+            // Send GAS as governance reward and persist it
+            const tx = await user.sendTransaction({
+                to: GovReward.target,
+                value: ethers.parseEther("7"),
+            });
+            await tx.wait();
+            await expect(
+                MockSysCall.call_onPersist(Governance)
+            ).to.be.not.reverted;
+        });
+
         it("Should transfer back correct reward if all conditions are met", async function () {
             // Register enough candidates and change consensus
             let signers = await ethers.getSigners();