e91d2f3290b120126d3fd5a74b2c98ec88d02cbe
Marius van der Wijden <m.vanderwijden@live.de>
2024-07-11 09:28:27 +0200

    core/txpool/blobpool: revert #29989, WLock on Nonce (#30142)

--- core/txpool/blobpool/blobpool.go
+++ core/txpool/blobpool/blobpool.go
@@ -1614,8 +1614,9 @@ func (p *BlobPool) SubscribeReannoTransactions(ch chan<- core.ReannoTxsEvent) ev
 // Nonce returns the next nonce of an account, with all transactions executable
 // by the pool already applied on top.
 func (p *BlobPool) Nonce(addr common.Address) uint64 {
-	p.lock.RLock()
-	defer p.lock.RUnlock()
+	// We need a write lock here, since state.GetNonce might write the cache.
+	p.lock.Lock()
+	defer p.lock.Unlock()
 
 	if txs, ok := p.index[addr]; ok {
 		return txs[len(txs)-1].nonce + 1
