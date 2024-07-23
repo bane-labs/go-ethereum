efb0d9189110896466b832c5cc2f910b77d09630
txhsl <799498265@qq.com>
2024-07-23 15:48:08 +0800

    systemcontract: add vote delegator and ut

---
+++ contracts/solidity/CommitteeMultiSig.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {GovernanceVote} from "./base/GovernanceVote.sol";
+import {ERC1967Utils, GovProxyUpgradeable} from "./base/GovProxyUpgradeable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+contract CommitteeMultiSig is GovernanceVote, GovProxyUpgradeable {
+    // Execute an operation that calls a function on the `target` contract
+    function execute(
+        address target,
+        bytes calldata data
+    )
+        external
+        needVote(keccak256(abi.encode(target)), keccak256(abi.encode(data)))
+        returns (bytes memory returndata)
+    {
+        returndata = Address.functionCall(target, data);
+    }
+}
