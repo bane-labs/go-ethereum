46b725c3c1cb99b1c79189e2e8c5e4efa6f90e73
Darioush Jalali <darioush.jalali@avalabs.org>
2024-08-01 07:06:43 -0700

    miner: remove outdated comment (#30248)

--- miner/worker.go
+++ miner/worker.go
@@ -695,8 +695,7 @@ func (w *worker) resultLoop() {
 
 // makeEnv creates a new environment for the sealing block.
 func (w *worker) makeEnv(parent *types.Header, header *types.Header, coinbase common.Address) (*environment, error) {
-	// Retrieve the parent state to execute on top and start a prefetcher for
-	// the miner to speed block sealing up a bit.
+	// Retrieve the parent state to execute on top.
 	state, err := w.chain.StateAt(parent.Root)
 	if err != nil {
 		return nil, err
