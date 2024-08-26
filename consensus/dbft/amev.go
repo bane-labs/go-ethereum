61ed2f897c4a0f08a10c1691280648d6d7c061d5
Anna Shaleva <shaleva.ann@nspcc.ru>
2024-08-26 11:58:52 +0300

    dbft: implement Envelope decoding
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

--- consensus/dbft/amev.go
+++ consensus/dbft/amev.go
@@ -1,30 +1,43 @@
 package dbft
 
 import (
+	"bytes"
+	"crypto/aes"
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/core/systemcontracts"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto/tpke"
-
-	"bytes"
 )
 
-const (
-	// txMinSize is the minimum size a single transaction can have.
-	// The size of a simple gas transfer with 1 gwei is 105 bytes, we use this as the minimum tx.
-	txMinSize = 105
+var (
+	// encryptedDataPrefix is the prefix of Envelope transaction's data. It's used to
+	// distinguish simple transactions that have GovernanceRewardProxy contract as a
+	// receiver from Envelope transactions carrying encrypted information inside. In
+	// future this prefix may be used for encrypted content versioning.
+	encryptedDataPrefix = []byte{0xff, 0xff, 0xff, 0xff}
+
+	// encryptedDataPrefixLen is the length of encryptedDataPrefix.
+	encryptedDataPrefixLen = len(encryptedDataPrefix)
+
+	// minEncryptedDataSize is the minimum size of encrypted data stored in the
+	// Envelope transaction. It consists of the constant-length prefix,
+	// constant-length CipherText and variable-length encrypted message. The size of
+	// a simple gas transfer with 1 gwei (105 bytes) is taken as a reference point
+	// for evaluation of variable-length part; it is padded to be even to the AES
+	// block size as required by AES encryption rules.
+	minEncryptedDataSize = encryptedDataPrefixLen + tpke.CipherTextSize + 105 + (aes.BlockSize - 105%aes.BlockSize)
 )
 
-// encryptDataPrefix is the prefix of Envelope tx data
-var encryptDataPrefix = []byte{0xff, 0xff, 0xff, 0xff}
-
-// isEnvelope checks whether a transaction is an envelope transaction,
-// including to address, data prefix and data length check.
+// isEnvelope checks whether a transaction is an Envelope transaction. The criteria
+// include receiver's address, data prefix and data length check.
 func isEnvelope(tx *types.Transaction) bool {
 	if tx.To() == nil || *(tx.To()) != systemcontracts.GovernanceRewardProxyHash {
 		return false
 	}
 
-	if len(tx.Data()) < txMinSize || !bytes.HasPrefix(tx.Data(), encryptDataPrefix) {
+	data := tx.Data()
+	if len(data) < minEncryptedDataSize || !bytes.HasPrefix(data, encryptedDataPrefix) {
 		return false
 	}
 
@@ -63,7 +76,18 @@ type envelopeData struct {
 	encryptedMsg []byte
 }
 
-func decodeEnvelopeData([]byte) (envelopeData, error) {
-	// TODO: implement RLP encoding/decoding for envelopeData
-	panic("TODO")
+// decodeEnvelopeData decodes envelopeData from the provided slice. It's a no-op to
+// pass not an Envelope's data.
+func decodeEnvelopeData(buf []byte) (envelopeData, error) {
+	var key = new(tpke.CipherText)
+	// It's guaranteed by Envelope definition that buf has a proper length.
+	_, err := key.FromBytes(buf[encryptedDataPrefixLen : encryptedDataPrefixLen+tpke.CipherTextSize])
+	if err != nil {
+		return envelopeData{}, fmt.Errorf("failed to decode TPKE cipher text: %w", err)
+	}
+	return envelopeData{
+		prefix:       buf[:encryptedDataPrefixLen],
+		encryptedKey: key,
+		encryptedMsg: buf[encryptedDataPrefixLen+tpke.CipherTextSize:],
+	}, nil
 }
