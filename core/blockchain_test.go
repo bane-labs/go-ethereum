cf413a7e1ad706606d8a43a142a999b6266618eb
rjl493456442 <garyrong0905@gmail.com>
2024-09-05 18:10:47 +0800

    core/state: state reader abstraction (#29761)
    This pull request introduces a state.Reader interface for state
    accessing.
    
    The interface could be implemented in various ways. It can be pure trie
    only reader, or the combination of trie and state snapshot. What's more,
    this interface allows us to have more flexibility in the future, e.g.
    the
    archive reader (for accessing archive state).
    
    Additionally, this pull request removes the following metrics
    
    - `chain/snapshot/account/reads`
    - `chain/snapshot/storage/reads`

--- core/blockchain_test.go
+++ core/blockchain_test.go
@@ -159,7 +159,7 @@ func testBlockChainImport(chain types.Blocks, blockchain *BlockChain) error {
 			}
 			return err
 		}
-		statedb, err := state.New(blockchain.GetBlockByHash(block.ParentHash()).Root(), blockchain.stateCache, nil)
+		statedb, err := state.New(blockchain.GetBlockByHash(block.ParentHash()).Root(), blockchain.statedb)
 		if err != nil {
 			return err
 		}
@@ -4271,7 +4271,7 @@ func TestEIP6110(t *testing.T) {
 			b.AddTx(tx)
 		}
 	})
-	chain, err := NewBlockChain(rawdb.NewMemoryDatabase(), nil, gspec, nil, engine, vm.Config{Tracer: logger.NewMarkdownLogger(&logger.Config{DisableStack: true}, os.Stderr).Hooks()}, nil)
+	chain, err := NewBlockChain(rawdb.NewMemoryDatabase(), nil, gspec, nil, engine, vm.Config{Tracer: logger.NewMarkdownLogger(&logger.Config{DisableStack: true}, os.Stderr).Hooks()}, nil, nil)
 	if err != nil {
 		t.Fatalf("failed to create tester chain: %v", err)
 	}
