7df38691aee2680a9ca373911a0414824b4f8d39
Sina M <1591639+s1na@users.noreply.github.com>
2024-10-17 06:51:47 +0200

    eth/tracers: various fixes (#30540)
    Breaking changes:
    
    - The ChainConfig was exposed to tracers via VMContext passed in
    `OnTxStart`. This is unnecessary specially looking through the lens of
    live tracers as chain config remains the same throughout the lifetime of
    the program. It was there so that native API-invoked tracers could
    access it. So instead we moved it to the constructor of API tracers.
    
    Non-breaking:
    
    - Change the default config of the tracers to be `{}` instead of nil.
    This way an extra nil check can be avoided.
    
    Refactoring:
    
    - Rename `supply` struct to `supplyTracer`.
    - Un-export some hook definitions.

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -615,6 +615,7 @@ var (
 	VMTraceJsonConfigFlag = &cli.StringFlag{
 		Name:     "vmtrace.jsonconfig",
 		Usage:    "Tracer configuration (JSON)",
+		Value:    "{}",
 		Category: flags.VMCategory,
 	}
 	// API options.
@@ -2020,13 +2021,8 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *ethconfig.Config) {
 	// VM tracing config.
 	if ctx.IsSet(VMTraceFlag.Name) {
 		if name := ctx.String(VMTraceFlag.Name); name != "" {
-			var config string
-			if ctx.IsSet(VMTraceJsonConfigFlag.Name) {
-				config = ctx.String(VMTraceJsonConfigFlag.Name)
-			}
-
 			cfg.VMTrace = name
-			cfg.VMTraceJsonConfig = config
+			cfg.VMTraceJsonConfig = ctx.String(VMTraceJsonConfigFlag.Name)
 		}
 	}
 }
@@ -2307,10 +2303,7 @@ func MakeChain(ctx *cli.Context, stack *node.Node, readonly bool) (*core.BlockCh
 	}
 	if ctx.IsSet(VMTraceFlag.Name) {
 		if name := ctx.String(VMTraceFlag.Name); name != "" {
-			var config json.RawMessage
-			if ctx.IsSet(VMTraceJsonConfigFlag.Name) {
-				config = json.RawMessage(ctx.String(VMTraceJsonConfigFlag.Name))
-			}
+			config := json.RawMessage(ctx.String(VMTraceJsonConfigFlag.Name))
 			t, err := tracers.LiveDirectory.New(name, config)
 			if err != nil {
 				Fatalf("Failed to create tracer %q: %v", name, err)
