5f2b25e14016ded9957c8416f26a2a269027921d
Roman Khimov <roman@nspcc.ru>
2025-02-11 18:16:05 +0300

    dbft: close the pool once the check is finished
    It has some goroutines spawned during Init and they keep the reference to pool
    which can affect memory consumption given that we create these pools just for
    temporary checks.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- consensus/dbft/dbft.go
+++ consensus/dbft/dbft.go
@@ -858,6 +858,7 @@ func (c *DBFT) verifyPreBlockCb(b dbft.PreBlock[common.Hash]) bool {
 	ethBlock := dbftBlock.ToEthBlock()
 
 	localPool := c.newLocalPool(parent)
+	defer localPool.CloseSilently()
 	errs := localPool.Add(dbftBlock.transactions, false, false)
 	for i, err := range errs {
 		if err != nil {
@@ -1093,6 +1094,7 @@ func (c *DBFT) processPreBlockCb(b dbft.PreBlock[common.Hash]) error {
 				return true
 			}
 		)
+		defer localPool.CloseSilently()
 		for i := range pre.transactions {
 			var isEnvelope = j < len(pre.envelopesData) && pre.envelopesData[j].index == i
 			if !isEnvelope || // pre.transactions[i] is not an envelope, use it as-is.
