1b17683a7dbd3886e23c1bb732d20abc080d9458
Delweng <delweng@gmail.com>
2025-03-28 15:15:13 +0800

    cmd/geth: update geth subcommand arguments (#31293)

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -160,7 +160,6 @@ var (
 		utils.VMTraceJsonConfigFlag,
 		utils.NetworkIdFlag,
 		utils.EthStatsURLFlag,
-		utils.NoCompactionFlag,
 		utils.GpoBlocksFlag,
 		utils.GpoPercentileFlag,
 		utils.GpoMaxGasPriceFlag,
