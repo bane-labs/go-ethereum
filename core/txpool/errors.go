e738a1da8d3264dcbc22d4a2b56a344379e9bf1e
rjl493456442 <garyrong0905@gmail.com>
2025-04-18 03:27:48 +0800

    eth: add tx to locals only if it has a chance of acceptance (#31618)
    This pull request improves error handling for local transaction submissions.
    
    Specifically, if a transaction fails with a temporary error but might be
    accepted later, the error will not be returned to the user; instead, the
    transaction will be tracked locally for resubmission.
    
    However, if the transaction fails with a permanent error (e.g., invalid
    transaction or insufficient balance), the error will be propagated to the user.
    
    These errors returned in the legacyPool are regarded as temporary failure:
    
    - `ErrOutOfOrderTxFromDelegated`
    - `txpool.ErrInflightTxLimitReached`
    - `ErrAuthorityReserved`
    - `txpool.ErrUnderpriced`
    - `ErrTxPoolOverflow`
    - `ErrFutureReplacePending`
    
    Notably, InsufficientBalance is also treated as a permanent error, as
    it’s highly unlikely that users will transfer funds into the sender account
    after submitting the transaction. Otherwise, users may be confused—seeing
    their transaction submitted but unaware that the sender lacks sufficient funds—and
    continue waiting for it to be included.
    
    ---------
    
    Co-authored-by: lightclient <lightclient@protonmail.com>

--- core/txpool/errors.go
+++ core/txpool/errors.go
@@ -16,7 +16,9 @@
 
 package txpool
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	// ErrAlreadyKnown is returned if the transactions is already contained
@@ -29,13 +31,18 @@ var (
 	// ErrBlockedSender is returned if the transaction sender is blocked by policy.
 	ErrBlockedSender = errors.New("blocked sender")
 
-	// ErrUnderpriced is returned if a transaction's gas price is below the minimum
-	// configured for the transaction pool.
+	// ErrUnderpriced is returned if a transaction's gas price is too low to be
+	// included in the pool. If the gas price is lower than the minimum configured
+	// one for the transaction pool, use ErrTxGasPriceTooLow instead.
 	ErrUnderpriced = errors.New("transaction underpriced")
 
 	// ErrReplaceUnderpriced is returned if a transaction is attempted to be replaced
 	// with a different one without the required price bump.
 	ErrReplaceUnderpriced = errors.New("replacement transaction underpriced")
+
+	// ErrTxGasPriceTooLow is returned if a transaction's gas price is below the
+	// minimum configured for the transaction pool.
+	ErrTxGasPriceTooLow = errors.New("transaction gas price below minimum")
 
 	// ErrAccountLimitExceeded is returned if a transaction would exceed the number
 	// allowed by a pool for a single account.
