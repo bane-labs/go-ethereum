bane-labs/go-ethereum

commit 1904272887d97872b85cb1588f293a552f8053ec

eth/downloader: refactor snap sync handling when pivot is nil (#535)

eth/downloader/downloader.go
@@ -486,21 +486,15 @@ func (d *Downloader) syncWithPeer(p *peerConnection, hash common.Hash, td, ttd *
 		if err != nil {
 			return err
 		}
-		if d.blockchain.TrieDB().Scheme() == rawdb.PathScheme && mode == SnapSync {
-			// Ensure the pivot is above the minimum full block threshold
-			pivotNumber := uint64(0)
-			if pivot != nil {
-				pivotNumber = pivot.Number.Uint64()
-			}
-			if pivotNumber < uint64(fsMinFullBlocks) {
-				block := d.blockchain.CurrentBlock()
-				if err = d.blockchain.TrieDB().Enable(block.Root); err != nil {
-					return fmt.Errorf("failed to enable trie db: %v", err)
+		if mode == SnapSync {
+			// If snap sync is not available (head below threshold), commit head and exit
+			if pivot == nil || pivot.Number.Uint64() < uint64(fsMinFullBlocks) {
+				header := d.blockchain.CurrentHeader()
+				if err := d.blockchain.SnapSyncCommitHead(header.Hash()); err != nil {
+					return err
 				}
-				log.Info("Disabling snap sync of the db on the path scheme due to the pivot header being positioned too low",
-					"pivot", pivotNumber)
-				<-time.After(3 * time.Second)
-				return errCanceled
+				d.committed.Store(true)
+				return nil
 			}
 		}
 	} else {

commit 0d90cfa106085534f80740b4ea9e4eefdc2e6b49

eth/downloader: fix ut failure caused by #535

eth/downloader/downloader_test.go
@@ -827,9 +827,14 @@ func testHighTDStarvationAttack(t *testing.T, protocol uint, mode SyncMode) {
 	tester := newTester(t)
 	defer tester.terminate()
 
-	chain := testChainBase.shorten(1)
-	tester.newPeer("attack", protocol, chain.blocks[1:])
-	if err := tester.sync("attack", big.NewInt(1000000), mode); err != errStallingPeer {
+	chain := testChainBase.shorten(200)
+	if mode == SnapSync {
+		// In snap sync, we need more blocks to avoid early termination due to a nil pivot.
+		tester.newPeer("attack", protocol, chain.blocks[1:])
+	} else {
+		tester.newPeer("attack", protocol, chain.blocks[200:])
+	}
+	if err := tester.sync("attack", big.NewInt(100000000), mode); err != errStallingPeer {
 		t.Fatalf("synchronisation error mismatch: have %v, want %v", err, errStallingPeer)
 	}
 }