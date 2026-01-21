29912e60a47540f4558fa0e7af8bf34826d32171
Csaba Kiraly <cskiraly@users.noreply.github.com>
2026-01-21 09:00:57 +0100

    core/txpool/legacypool: reset gauges on clear (#33654)
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>
    Co-authored-by: rjl493456442 <garyrong0905@gmail.com>

--- core/txpool/legacypool/legacypool.go
+++ core/txpool/legacypool/legacypool.go
@@ -1970,6 +1970,10 @@ func (pool *LegacyPool) Clear() {
 	pool.queue = newQueue(pool.config, pool.signer)
 	pool.pendingNonces = newNoncer(pool.currentState)
 
+	// Reset gauges
+	pendingGauge.Update(0)
+	queuedGauge.Update(0)
+	slotsGauge.Update(0)
 	pendingAddrsGauge.Update(0)
 	queuedAddrsGauge.Update(0)
 }
