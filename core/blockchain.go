310ac6a55e9ab13df6ca0fdcffc8a40c6615f150
rjl493456442 <garyrong0905@gmail.com>
2026-03-04 18:17:47 +0800

    core, ethdb, triedb: add batch close (#33708)
    Pebble maintains a batch pool to recycle the batch object. Unfortunately
    batch object must be
    explicitly returned via `batch.Close` function. This PR extends the
    batch interface by adding
    the close function and also invoke batch.Close in some critical code
    paths.
    
    Memory allocation must be measured before merging this change. What's
    more, it's an open
    question that whether we should apply batch.Close as much as possible in
    every invocation.

--- core/blockchain.go
+++ core/blockchain.go
@@ -1284,6 +1284,8 @@ func (bc *BlockChain) ExportN(w io.Writer, first uint64, last uint64) error {
 func (bc *BlockChain) writeHeadBlock(block *types.Block) {
 	// Add the block to the canonical chain number scheme and mark as the head
 	batch := bc.db.NewBatch()
+	defer batch.Close()
+
 	rawdb.WriteHeadHeaderHash(batch, block.Hash())
 	rawdb.WriteHeadFastBlockHash(batch, block.Hash())
 	rawdb.WriteCanonicalHash(batch, block.Hash(), block.NumberU64())
@@ -1677,6 +1679,8 @@ func (bc *BlockChain) writeBlockWithState(block *types.Block, receipts []*types.
 		batch = bc.db.NewBatch()
 		start = time.Now()
 	)
+	defer batch.Close()
+
 	rawdb.WriteTd(batch, block.Hash(), block.NumberU64(), externTd)
 	rawdb.WriteBlock(batch, block)
 	rawdb.WriteReceipts(batch, block.Hash(), block.NumberU64(), receipts)
@@ -2702,6 +2706,8 @@ func (bc *BlockChain) reorg(oldHead *types.Header, newHead *types.Header) error 
 	// Delete useless indexes right now which includes the non-canonical
 	// transaction indexes, canonical chain indexes which above the head.
 	batch := bc.db.NewBatch()
+	defer batch.Close()
+
 	for _, tx := range types.HashDifference(deletedTxs, rebirthTxs) {
 		rawdb.DeleteTxLookupEntry(batch, tx)
 	}
