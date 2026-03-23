07e0142327d462045ec53bb0bcdda53d33376522
Felföldi Zsolt <zsfelfoldi@gmail.com>
2026-03-23 15:29:53 +0100

    core, core/vm: implement EIP-7708 (#33645)
    This PR implements EIP-7708 according to the latest "rough consensus":
    
    https://github.com/ethereum/EIPs/pull/9003
    https://github.com/etan-status/EIPs/blob/fl-ethlogs/EIPS/eip-7708.md
    
    ---------
    
    Co-authored-by: Jared Wasinger <j-wasinger@hotmail.com>
    Co-authored-by: raxhvl <raxhvl@users.noreply.github.com>
    Co-authored-by: Gary Rong <garyrong0905@gmail.com>

--- core/state_transition.go
+++ core/state_transition.go
@@ -623,6 +623,9 @@ func (st *stateTransition) execute() (*ExecutionResult, error) {
 			st.evm.AccessEvents.AddAccount(st.evm.Context.Coinbase, true, math.MaxUint64)
 		}
 	}
+	if rules.IsAmsterdam {
+		st.evm.StateDB.EmitLogsForBurnAccounts()
+	}
 	return &ExecutionResult{
 		UsedGas:    st.gasUsed(),
 		MaxUsedGas: peakGasUsed,
