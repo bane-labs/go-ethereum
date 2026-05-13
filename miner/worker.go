ba3ec4e4925f3a54ac49fd48452c8e867faf18b7
rjl493456442 <garyrong0905@gmail.com>
2026-05-13 20:38:47 +0800

    core/state: track the block-level accessList (#34803)
    This PR extends the journal to track the pre-transaction values of
    mutated balances, nonces, and code.
    
    At the end of the transaction, these values are used to filter out no-op
    changes, such as balance transitions from a-> b->a. These changes are
    excluded from the block-level access list.
    
    Additionally, there is a dedicated `bal.ConstructionBlockAccessList`
    objects for gathering the state reads and writes within the current
    transaction. These state writes will be keyed by the block accessList
    index.
    
    ---------
    
    Co-authored-by: jwasinger <j-wasinger@hotmail.com>

--- miner/worker.go
+++ miner/worker.go
@@ -170,7 +170,7 @@ func (miner *Miner) generateWork(ctx context.Context, genParam *generateParams, 
 		// otherwise, fill the block with the current transactions from the txpool
 		if genParam.forceOverrides && len(genParam.overrideTxs) > 0 {
 			for _, tx := range genParam.overrideTxs {
-				work.state.SetTxContext(tx.Hash(), work.tcount)
+				work.state.SetTxContext(tx.Hash(), work.tcount, uint32(work.tcount+1))
 				if err := miner.commitTransaction(ctx, work, tx); err != nil {
 					// all passed transactions HAVE to be valid at this point
 					return &newPayloadResult{err: err}
@@ -211,7 +211,7 @@ func (miner *Miner) generateWork(ctx context.Context, genParam *generateParams, 
 	}
 
 	// Collect consensus-layer requests if Prague is enabled.
-	requests, err := core.PostExecution(ctx, miner.chainConfig, work.header.Number, work.header.Time, allLogs, work.evm)
+	requests, err := core.PostExecution(ctx, miner.chainConfig, work.header.Number, work.header.Time, allLogs, work.evm, uint32(work.tcount+1))
 	if err != nil {
 		return &newPayloadResult{err: err}
 	}
@@ -526,7 +526,7 @@ func (miner *Miner) commitTransactions(ctx context.Context, env *environment, pl
 			continue
 		}
 		// Start executing the transaction
-		env.state.SetTxContext(tx.Hash(), env.tcount)
+		env.state.SetTxContext(tx.Hash(), env.tcount, uint32(env.tcount+1))
 
 		err := miner.commitTransaction(ctx, env, tx)
 		switch {
