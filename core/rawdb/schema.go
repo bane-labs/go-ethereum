c96ff66798fa6ebebc1413ce0c215fd567b05541
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2026-06-15 03:09:41 -0500

    eth/downloader, eth/protocols/snap: freeze pivot once state is downloaded (#35155)

--- core/rawdb/schema.go
+++ core/rawdb/schema.go
@@ -104,10 +104,6 @@ var (
 	// snapSyncStatusFlagKey flags that status of snap sync.
 	snapSyncStatusFlagKey = []byte("SnapSyncStatus")
 
-	// generateTriePartitionDonePrefix stores the subtree root hash of each
-	// triedb.GenerateTrie partition once it finishes.
-	generateTriePartitionDonePrefix = []byte("gtd") // generateTriePartitionDonePrefix + partition byte -> subtree root hash
-
 	// beaconSyncTrustedHeadKey tracks the latest trusted head for beacon sync, which is supposed to be the latest finalized block.
 	beaconSyncTrustedHeadKey = []byte("BeaconSyncTrustedHead")
 
@@ -476,9 +472,4 @@ func trienodeHistoryIndexBlockKey(addressHash common.Hash, path []byte, blockID 
 // transitionStateKey = transitionStatusKey + hash
 func transitionStateKey(hash common.Hash) []byte {
 	return append(VerkleTransitionStatePrefix, hash.Bytes()...)
-}
-
-// generateTriePartitionDoneKey = generateTriePartitionDonePrefix + partition (single byte).
-func generateTriePartitionDoneKey(partition byte) []byte {
-	return append(generateTriePartitionDonePrefix, partition)
 }
