efa8985d40a0bdd20f634bcbf20592610403bec6
Hu Shili <799498265@qq.com>
2026-08-20 12:04:05 +0800

    cmd: add back missing flag setting for miner unlock

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1713,6 +1713,9 @@ func SetNodeConfig(ctx *cli.Context, cfg *node.Config) {
 	if ctx.IsSet(USBFlag.Name) {
 		cfg.USB = ctx.Bool(USBFlag.Name)
 	}
+	if ctx.IsSet(InsecureUnlockAllowedFlag.Name) {
+		cfg.InsecureUnlockAllowed = ctx.Bool(InsecureUnlockAllowedFlag.Name)
+	}
 	if ctx.IsSet(DBEngineFlag.Name) {
 		dbEngine := ctx.String(DBEngineFlag.Name)
 		if dbEngine != "leveldb" && dbEngine != "pebble" {
